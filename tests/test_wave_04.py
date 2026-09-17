79014b28f51407893c0e3a3671345f8e3846e423
younnlei <younl.eain@gmail.com>
2026-09-18 03:02:22 +0630

    wave4: implement get_available_recs 3 tests passed
    Co-authored-by: Kage Edwards <kage.edwards1992@gmail.com>

--- tests/test_wave_04.py
+++ tests/test_wave_04.py
@@ -2,7 +2,7 @@ import pytest
 from viewing_party.party import *
 from tests.test_constants import *
 
-@pytest.mark.skip()
+
 def test_get_available_friend_rec():
     # Arrange
     amandas_data = clean_wave_4_data()
@@ -10,13 +10,15 @@ def test_get_available_friend_rec():
     # Act
     recommendations = get_available_recs(amandas_data)
 
+    print(recommendations)
+
     # Assert
     assert len(recommendations) == 2
     assert HORROR_1b in recommendations
     assert FANTASY_4b in recommendations
     assert amandas_data == clean_wave_4_data()
 
-@pytest.mark.skip()
+
 def test_no_available_friend_recs():
     # Arrange
     amandas_data = {
@@ -38,7 +40,7 @@ def test_no_available_friend_recs():
     # Assert
     assert len(recommendations) == 0
 
-@pytest.mark.skip()
+
 def test_no_available_friend_recs_watched_all():
     # Arrange
     amandas_data = {
--- viewing_party/party.py
+++ viewing_party/party.py
@@ -118,6 +118,19 @@ def get_friends_unique_watched(user_data: dict[str, list]) -> list[dict]:
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
 
+def get_available_recs(user_data: dict[str, list]) -> list[dict]:
+    recommendations = []
+
+    for friend in user_data["friends"]:
+        for movie in friend["watched"]:
+            if movie["host"] in user_data["subscriptions"]:
+                if movie not in user_data["watched"]:
+                    if movie not in recommendations:
+                        recommendations.append(movie)
+
+    return recommendations
+
+
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
